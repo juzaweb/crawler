92cb1315d26908f7c75998265548d0193fded446
Juzaweb <dangtheanh16@gmail.com>
2024-02-09 13:56:58 +0700

    :+1: Filter content by categories

--- src/Models/CustomCategory.php
+++ src/Models/CustomCategory.php
@@ -10,7 +10,10 @@
 
 namespace Juzaweb\Crawler\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 use Juzaweb\CMS\Models\Model;
 use Juzaweb\CMS\Models\User;
 
@@ -20,18 +23,18 @@ use Juzaweb\CMS\Models\User;
  * @property int $id
  * @property string $name
  * @property int $user_id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \Juzaweb\CMS\Models\User $user
- * @method static \Illuminate\Database\Eloquent\Builder|\Juzaweb\Scrawler\Models\CustomCategory newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\Juzaweb\Scrawler\Models\CustomCategory newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\Juzaweb\Scrawler\Models\CustomCategory query()
- * @method static \Illuminate\Database\Eloquent\Builder|\Juzaweb\Scrawler\Models\CustomCategory whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Juzaweb\Scrawler\Models\CustomCategory whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Juzaweb\Scrawler\Models\CustomCategory whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Juzaweb\Scrawler\Models\CustomCategory whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Juzaweb\Scrawler\Models\CustomCategory whereUserId($value)
- * @mixin \Eloquent
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read User $user
+ * @method static Builder|\Juzaweb\Scrawler\Models\CustomCategory newModelQuery()
+ * @method static Builder|\Juzaweb\Scrawler\Models\CustomCategory newQuery()
+ * @method static Builder|\Juzaweb\Scrawler\Models\CustomCategory query()
+ * @method static Builder|\Juzaweb\Scrawler\Models\CustomCategory whereCreatedAt($value)
+ * @method static Builder|\Juzaweb\Scrawler\Models\CustomCategory whereId($value)
+ * @method static Builder|\Juzaweb\Scrawler\Models\CustomCategory whereName($value)
+ * @method static Builder|\Juzaweb\Scrawler\Models\CustomCategory whereUpdatedAt($value)
+ * @method static Builder|\Juzaweb\Scrawler\Models\CustomCategory whereUserId($value)
+ * @mixin Eloquent
  */
 class CustomCategory extends Model
 {
