0fd3e3cf295bb7dbad7ccc9653cccdf42cb32e0b
Juzaweb <dangtheanh16@gmail.com>
2024-02-13 21:21:53 +0700

    :+1: Content event

---
+++ src/Events/ContentCreated.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * JUZAWEB CMS - Laravel CMS for Your Project
+ *
+ * @package    juzaweb/cms
+ * @author     The Anh Dang
+ * @link       https://juzaweb.com
+ * @license    GNU V2
+ */
+
+namespace Juzaweb\Crawler\Events;
+
+use Juzaweb\Crawler\Interfaces\CrawlerContentEntity;
+
+class ContentCreated
+{
+    public function __construct(public CrawlerContentEntity $content)
+    {
+    }
+}
